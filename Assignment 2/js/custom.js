d4a1a65fc8e46c50b9faeeec935ee31a49ce3db5
asish24-git <raoasishakr2426@gmail.com>
2021-05-28 00:23:44 +0530

    first commit

---
+++ Assignment 2/js/custom.js
@@ -0,0 +1,16 @@
+function showme()
+{
+    var menubar = document.getElementById("menu");
+
+    if(menubar.style.display == "none")
+
+    {
+        menubar.style.display="block";
+
+    }
+
+    else
+    {
+        menubar.style.display="none"
+    }
+}
